440f905e1cd119e74761e9a5bafac42483aa7ce3
ethanjharrington012 <ethanjharrington012@gmail.com>
2023-03-24 19:53:17 -0700

    first commit

---
+++ ethan_module.py
@@ -0,0 +1,13 @@
+from math import pi
+
+def sq_ft_house(l, w):
+    sq_ft = l*w
+    return f"\nyour house is {sq_ft} square feet"
+
+
+
+def circles_circumference(radius):
+
+    circumference = 2*pi*radius
+    return f"\nYour circles circumference is: {circumference}"
+
